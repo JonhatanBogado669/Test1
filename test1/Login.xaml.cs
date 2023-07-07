d2ed1e61bc793a4ceac79fe20c82b053b4df7ca5
Jonhatan Bogado <jonhatanbogado@gmail.com>
2023-07-07 16:51:43 -0400

    separación de login, menú y reset password en ventanas propias

---
+++ test1/Login.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System.Collections.ObjectModel;
+using System.Collections;
+using test1.models;
+using test1;
+
+
+namespace test1
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+
+     
+    
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        /////////////////////////////////Login/////////////////////////////////////
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernametextBox.Text;
+            string password = PasswordtextBox.Password;
+
+            if (string.IsNullOrEmpty(UsernametextBox.Text) || string.IsNullOrEmpty(PasswordtextBox.Password))
+            {
+                MessageBox.Show("Por favor, ingrese su nombre de usuario y contraseña.", "Inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (LoginU(UsernametextBox.Text, PasswordtextBox.Password))
+            {
+                // Inicio de sesión exitoso, realizar acciones según el rol del usuario
+                string role = GetUserRole(UsernametextBox.Text);
+                switch (role)
+                {
+                    case "Admin":
+                        // Lógica para el rol de administrador
+                        UsernametextBox.Text = null;
+                        PasswordtextBox.Password = null;
+                        MainWindow main = new MainWindow();
+                        main.Show();
+                        Close();
+                        MessageBox.Show("¡Inicio de sesión exitoso como administrador!", "Inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+                    case "Usuario":
+                        // Lógica para el rol de usuario normal
+                        MessageBox.Show("¡Inicio de sesión exitoso como usuario!", "Inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Information);
+                        break;
+                    default:
+                        MessageBox.Show("El usuario no tiene un rol válido.", "Inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nombre de usuario o contraseña incorrectos.", "Inicio de sesión", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        private bool LoginU(string username, string password)
+        {
+            try
+            {
+                string hashedPassword = HashPassword(password);
+                Conexion.conectar();
+
+                string cadena = "SELECT COUNT(*) FROM users WHERE username = '" + UsernametextBox.Text + "' AND password = '" + PasswordtextBox.Password + "'";
+                MySqlCommand cmd = new MySqlCommand(cadena, Conexion.conectar());
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                return count > 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al intentar iniciar sesión: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+
+        private string GetUserRole(string username)
+        {
+            try
+            {
+                Conexion.conectar();
+
+                string cadena = "SELECT role FROM users WHERE username = '" + UsernametextBox.Text + "'";
+                MySqlCommand cmd = new MySqlCommand(cadena, Conexion.conectar());
+                string role = cmd.ExecuteScalar()?.ToString();
+                return role;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener el rol del usuario: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        /////////////////////////Reseteo de Contraseña///////////////////////////
+        private void ForgotPasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernametextBox.Text;
+
+            if (string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("Por favor, ingrese su nombre de usuario.", "Restablecimiento de contraseña", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            string email = GetEmailByUsername(username);
+            if (string.IsNullOrEmpty(email))
+            {
+                MessageBox.Show("No se encontró ninguna dirección de correo electrónico asociada a ese nombre de usuario.", "Restablecimiento de contraseña", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (username != null && email != null)
+            {
+                //LoginGrid.Visibility = Visibility.Collapsed;
+                //tabControl.Visibility = Visibility.Collapsed;
+                ResetPassword respass = new ResetPassword();
+                respass.Show();
+            }
+        }
+        private string GetEmailByUsername(string username)
+        {
+            try
+            {
+
+                string query = "SELECT correo FROM users WHERE username = '" + username + "'";
+                MySqlCommand cmd = new MySqlCommand(query, Conexion.conectar());
+                string email = cmd.ExecuteScalar()?.ToString();
+                return email;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener la dirección de correo electrónico: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+    }
+}
